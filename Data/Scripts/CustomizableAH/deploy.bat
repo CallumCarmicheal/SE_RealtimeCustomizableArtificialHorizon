a41e40f8417358a85f7215e96e8364ded7ef471e
Callum Carmicheal <callumcarmicheal@gmail.com>
2021-01-04 20:37:43 +0000

    Added deployment script to deploy to appdata mods

---
+++ Data/Scripts/CustomizableAH/deploy.bat
@@ -0,0 +1,8 @@
+mkdir %appdata%\SpaceEngineers\Mods\CustomizableAH\Data\Scripts\CustomizableAH\ 2> NUL
+copy "..\..\..\metadata.mod" "%appdata%\SpaceEngineers\Mods\CustomizableAH"
+copy Logger.cs "%appdata%\SpaceEngineers\Mods\CustomizableAH\Data\Scripts\CustomizableAH\Logger.cs"
+copy TSSArtificialHorizon.cs "%appdata%\SpaceEngineers\Mods\CustomizableAH\Data\Scripts\CustomizableAH\TSSArtificialHorizon.cs.cs"
+copy TSSBlock.cs "%appdata%\SpaceEngineers\Mods\CustomizableAH\Data\Scripts\CustomizableAH\TSSBlock.cs"
+copy TSSCommon.cs "%appdata%\SpaceEngineers\Mods\CustomizableAH\Data\Scripts\CustomizableAH\TSSCommon.cs"
+copy TSSGravity.cs "%appdata%\SpaceEngineers\Mods\CustomizableAH\Data\Scripts\CustomizableAH\TSSGravity.cs"
+copy TSSVelocity.cs "%appdata%\SpaceEngineers\Mods\CustomizableAH\Data\Scripts\CustomizableAH\TSSVelocity.cs"
